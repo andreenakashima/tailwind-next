c56e3d16641e942d6f718ef0b27c63fd5bb5361c
andreenakashima <andreenakashima@gmail.com>
2023-08-04 19:29:48 -0300

    Criando abas com radix tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import SettingTabs from "@/componenst/SettingsTabs";
 
 export default function Home() {
-	return <h1 className="text-xl">Hello Tailwind</h1>;
+	return (
+		<>
+			<h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+
+			<SettingTabs />
+		</>
+	);
 }
