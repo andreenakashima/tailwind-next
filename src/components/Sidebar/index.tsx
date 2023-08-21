6f2a487dee09441dfb62f0f45b00a0998007cd77
andreenakashima <andreenakashima@gmail.com>
2023-08-21 14:59:18 -0300

    Abertura da sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
 	BarChart,
 	CheckSquare,
@@ -5,6 +7,7 @@ import {
 	Flag,
 	Home,
 	LifeBuoy,
+	Menu,
 	Search,
 	SquareStack,
 	Users,
@@ -13,42 +16,57 @@ import Logo from "./Logo";
 import NavItem from "./NavItem";
 import UsedSpaceWidget from "./UsedSpaceWidget";
 import Profile from "./Profile";
+import * as Collapsible from "@radix-ui/react-collapsible";
 import * as Input from "../Input";
+import { Button } from "../Button";
 
-const Sidebar = () => {
+export const Sidebar = () => {
 	return (
-		<aside className="fixed top-0 left-0 right-0 bottom-0 z-20 bg-white border-b  flex flex-col gap-6 p-4 border-zinc-200 lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:relative">
-			<Logo />
+		<Collapsible.Root className="fixed left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 data-[state=open]:bottom-0 lg:right-auto  lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:data-[state=closed]:bottom-0">
+			<div className="flex items-center justify-between">
+				<Logo />
 
-			<Input.Root>
-				<Input.Prefix>
-					<Search className="w-5 h-5 text-zinc-500" />
-				</Input.Prefix>
-				<Input.Control placeholder="Search" />
-			</Input.Root>
+				<Collapsible.Trigger asChild className="lg:hidden">
+					<Button variant="ghost">
+						<Menu className="w-6 h-6" />
+					</Button>
+				</Collapsible.Trigger>
+			</div>
 
-			<nav className="space-y-0.5">
-				<NavItem title="Home" icon={Home} />
-				<NavItem title="Dashboard" icon={BarChart} />
-				<NavItem title="Projects" icon={SquareStack} />
-				<NavItem title="Tasks" icon={CheckSquare} />
-				<NavItem title="Reporting" icon={Flag} />
-				<NavItem title="Users" icon={Users} />
-			</nav>
+			<Collapsible.Content
+				forceMount
+				className="flex flex-1 flex-col gap-6 data-[state=closed]:hidden lg:data-[state=closed]:flex"
+			>
+				<Input.Root>
+					<Input.Prefix>
+						<Search className="w-5 h-5 text-zinc-500" />
+					</Input.Prefix>
+					<Input.Control placeholder="Search" />
+				</Input.Root>
 
-			<div className="mt-auto flex flex-col gap-6">
 				<nav className="space-y-0.5">
-					<NavItem title="Support" icon={LifeBuoy} />
-					<NavItem title="Settings" icon={Cog} />
+					<NavItem title="Home" icon={Home} />
+					<NavItem title="Dashboard" icon={BarChart} />
+					<NavItem title="Projects" icon={SquareStack} />
+					<NavItem title="Tasks" icon={CheckSquare} />
+					<NavItem title="Reporting" icon={Flag} />
+					<NavItem title="Users" icon={Users} />
 				</nav>
 
-				<UsedSpaceWidget />
+				<div className="mt-auto flex flex-col gap-6">
+					<nav className="space-y-0.5">
+						<NavItem title="Support" icon={LifeBuoy} />
+						<NavItem title="Settings" icon={Cog} />
+					</nav>
 
-				<div className="h-px bg-zinc-200" />
+					<UsedSpaceWidget />
 
-				<Profile />
-			</div>
-		</aside>
+					<div className="h-px bg-zinc-200" />
+
+					<Profile />
+				</div>
+			</Collapsible.Content>
+		</Collapsible.Root>
 	);
 };
 
