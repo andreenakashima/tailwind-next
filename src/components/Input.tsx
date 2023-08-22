d18dbca79de450e1eb116d7d797d63e5a0d9f8ef
andreenakashima <andreenakashima@gmail.com>
2023-08-22 09:56:47 -0300

    Tema dark sidebar

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,5 @@
 import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
 type PrefixProps = ComponentProps<"div">;
 export const Prefix = (props: PrefixProps) => {
@@ -9,7 +10,7 @@ type ControlProps = ComponentProps<"input">;
 export const Control = (props: ControlProps) => {
 	return (
 		<input
-			className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
+			className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none dark:placeholder-zinc-400 dark:text-zinc-100"
 			{...props}
 		/>
 	);
@@ -20,7 +21,12 @@ type RootProps = ComponentProps<"div">;
 export const Root = (props: RootProps) => {
 	return (
 		<div
-			className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 py-2 px-3 shadow-sm focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100"
+			className={twMerge(
+				"flex w-full items-center gap-2 rounded-lg border border-zinc-300 py-2 px-3 shadow-sm",
+				"focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100",
+				"dark:border-zinc-700 dark:bg-zinc-800 dark:focus-within:ring-violet-500/10 dark:focus-within:border-violet-500",
+				props.className
+			)}
 			{...props}
 		/>
 	);
--- src/components/Sidebar/UsedSpaceWidget.tsx
+++ src/components/Sidebar/UsedSpaceWidget.tsx
@@ -1,29 +1,29 @@
-const UsedSpaceWidget = () => {
+export const UsedSpaceWidget = () => {
 	return (
-		<div className="flex flex-col gap-4 px-4 py-5 bg-violet-50 rounded-lg">
+		<div className="flex flex-col gap-4 px-4 py-5 bg-violet-50 rounded-lg dark:bg-zinc-800">
 			<div className="space-y-1">
-				<span className="text-sm/5 font-medium text-violet-700">
+				<span className="text-sm/5 font-medium text-violet-700 dark:text-zinc-100">
 					Used Space
 				</span>
-				<p className="text-sm/5 text-violet-500">
+				<p className="text-sm/5 text-violet-500 dark:text-zinc-400">
 					Your team has used 80% of your available space. Need more?
 				</p>
 			</div>
 
-			<div className="h-2 rounded-full bg-violet-100">
-				<div className="h-2 w-4/5 rounded-full bg-violet-600" />
+			<div className="h-2 rounded-full bg-violet-100 dark:bg-zinc-600">
+				<div className="h-2 w-4/5 rounded-full bg-violet-600 dark:bg-violet-400" />
 			</div>
 
 			<div className="space-x-3">
 				<button
 					type="button"
-					className="text-sm font-medium text-violet-500 hover:text-violet-700"
+					className="text-sm font-medium text-violet-500 hover:text-violet-700 dark:text-violet-300 dark:hover:to-violet-200"
 				>
 					Dismiss
 				</button>
 				<button
 					type="button"
-					className="text-sm font-medium text-violet-700 hover:text-violet-900"
+					className="text-sm font-medium text-violet-700 hover:text-violet-900 dark:text-violet-300 dark:hover:text-zinc-100"
 				>
 					Upgrade plan
 				</button>
@@ -31,5 +31,3 @@ const UsedSpaceWidget = () => {
 		</div>
 	);
 };
-
-export default UsedSpaceWidget;
